4ebff594e66fde5d8189cf545374ef84887dcfd9
aseelr7 <aseelr95@gmail.com>
2025-08-19 04:11:50 -0400

    Add a new for loop

--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -1,5 +1,8 @@
 public class Sample {
     public static void main(String[] args) {
         System.out.println("aseel");
+        for (int i = 0 ; i < 10 ; i++){
+            System.out.println(i);
+        }
     }
 }
