19f8ba31d9e4f251fc37c99773ac49c1ba1c1280
aseelr7 <aseelr95@gmail.com>
2025-08-28 04:38:07 -0400

    added xmls for parallel and cross browser testing alongside property files

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -4,21 +4,47 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 public class BaseClass {
     public WebDriver driver;
     public Logger logger;
+    public Properties p;
 
     @BeforeClass
-    public void setup(){
+    @Parameters({"os","browser"})
+    public void setup(String os, String br) throws IOException {
+
+        // Loading Properties
+        FileReader file = new FileReader("./src//test//resources//config.properties");
+        p = new Properties();
+        p.load(file);
+
+
+
         logger = LogManager.getLogger(this.getClass());
-        driver = new ChromeDriver();
+
+        switch (br.toLowerCase()){
+            case "chrome" : driver = new ChromeDriver();
+            break;
+            case "firefox" : driver = new FirefoxDriver();
+            break;
+            case "edge" : driver = new EdgeDriver();
+            break;
+            default:
+                System.out.println("Invalid driver"); return;
+        }
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-        driver.get("https://opencart.antropy.co.uk/");
+        driver.get(p.getProperty("appURL1"));
         driver.manage().window().maximize();
     }
 
