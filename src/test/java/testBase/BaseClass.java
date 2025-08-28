7980e4a0ebee68cc4f6abe7ebbd44bf16d46fc5c
aseelr7 <aseelr95@gmail.com>
2025-08-28 03:57:26 -0400

    added configuration for Log4j and some logging steps in tc001

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -1,20 +1,23 @@
 package testBase;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-
 import java.time.Duration;
 
 public class BaseClass {
     public WebDriver driver;
+    public Logger logger;
+
     @BeforeClass
     public void setup(){
+        logger = LogManager.getLogger(this.getClass());
         driver = new ChromeDriver();
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-
         driver.get("https://opencart.antropy.co.uk/");
         driver.manage().window().maximize();
     }
--- src/test/java/testCases/TC001_AccountRegistration.java
+++ src/test/java/testCases/TC001_AccountRegistration.java
@@ -10,23 +10,33 @@ public class TC001_AccountRegistration extends BaseClass {
 
     @Test
     public void verify_account_registration() {
+     try {
+         logger.info("Test Case Started");
 
-        HomePage hp = new HomePage(driver);
-        hp.clickMyAccount();
-        hp.clickRegister();
+         HomePage hp = new HomePage(driver);
+         hp.clickMyAccount();
+         hp.clickRegister();
+         logger.info("Clicked on my account and registration");
 
 
-        RegistrationPage rp = new RegistrationPage(driver);
-        rp.setTxtFirstname("Jennie");
-        rp.setTxtLastname("Johnson");
-        rp.setTxtEmail("asdf@gmail.com");
-        rp.setTxtTelephone("123456");
-        rp.setTxtPassword("1234441");
-        rp.setTxtConfirm("1234441");
-        rp.setTxtCaptcha();
-        rp.clickAgreeCheckbox();
-        rp.clickContinueButton();
+         RegistrationPage rp = new RegistrationPage(driver);
+         rp.setTxtFirstname("Jennie");
+         rp.setTxtLastname("Johnson");
+         rp.setTxtEmail("asdf@gmail.com");
+         rp.setTxtTelephone("123456");
+         rp.setTxtPassword("1234441");
+         rp.setTxtConfirm("1234441");
+         rp.setTxtCaptcha();
+         rp.clickAgreeCheckbox();
+         rp.clickContinueButton();
+         logger.info("provided credentials");
 
-        Assert.assertEquals(rp.getConfirmation(), "Your Account Has Been Created!");
+         Assert.assertEquals(rp.getConfirmation(), "Your Account Has Been Created!");
+     } catch (Exception e){
+         logger.error("Test Failed");
+         logger.debug("Debug Logs : ");
+         Assert.fail();
+     }
+
     }
 }
